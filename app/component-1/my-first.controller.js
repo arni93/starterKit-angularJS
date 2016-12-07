299c461741b721bdd7308057226d518e50af842b
Arnold Woznica <arnold.woznica@capgemini.com>
2016-12-07 16:33:31 +0100

    zmienione nazwy w dialogA

--- app/component-1/my-first.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('app.component1').controller('MyFirstController', function($scope, $http, $modal, books){
-   'use strict';
-
-
-}).controller('MyModalController', function($scope, $modalInstance, selectedBook){
-    'use strict';
-
-})
